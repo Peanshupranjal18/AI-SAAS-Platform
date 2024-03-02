c052b5fe165477bf9814257a352c516beceb869c
Pranshu Pranjal <pprnch@gmail.com>
2024-03-02 19:51:51 +0530

    Commit

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,12 @@
 import { authMiddleware } from "@clerk/nextjs";
-
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  return NextResponse.redirect(new URL('/home', request.url))
+}
+ 
 export default authMiddleware({
   publicRoutes: ["/", "/api/webhook"],
 });
